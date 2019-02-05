b69647c5a54425dc98b171a3ca775b0c4683a540
Tom Berman <tjcberman@gmail.com>
2019-02-05 10:43:39 +0000

    Fix up look sharp

--- static-src/js/stats.js
+++ static-src/js/stats.js
@@ -42,52 +42,59 @@ function randomArray(length, max) {
     });
 }
 
-for (var i = 0; i < 6; i++) {
-    var ctx = document.getElementById("stats-" + i).getContext("2d");
-    var type, dataType;
+function drawChart(element, max) {
+  var ctx = element.getContext("2d");
+  var type, dataType;
 
-    if (i !== 1 && i !== 4) {
-        type = "line";
-        dataType = getGeneratedLineData(randomArray(30, 1000));
-    } else {
-        type = "bar";
-        dataType = getGeneratedBarData(randomArray(30, 1000));
-    }
+  if (i !== 1 && i !== 4) {
+      type = "line";
+      dataType = getGeneratedLineData(randomArray(50, max));
+  } else {
+      type = "bar";
+      dataType = getGeneratedBarData(randomArray(50, max));
+  }
 
-    new Chart(ctx, {
-        type: type,
-        data: dataType,
-        scaleShowVerticalLines: false,
-        scaleGridLineColor: "black",
-        options: {
-            responsive: true,
-            maintainAspectRatio: false,
-            elements: {
-                line: {
-                    tension: 0
-                }
-            },
-            legend: {
-                display: false
-            },
-            scales: {
-                yAxes: [
-                    {
-                        ticks: {
-                            fontColor: "#444363",
-                            fontSize: 12
-                        }
-                    }
-                ],
-                xAxes: [
-                    {
-                        ticks: {
-                            fontColor: "#444363",
-                            fontSize: 12
-                        }
-                    }
-                ]
-            }
-        }
-    });
+  new Chart(ctx, {
+      type: type,
+      data: dataType,
+      scaleShowVerticalLines: false,
+      scaleGridLineColor: "black",
+      options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          elements: {
+              line: {
+                  tension: 0
+              }
+          },
+          legend: {
+              display: false
+          },
+          scales: {
+              yAxes: [
+                  {
+                      ticks: {
+                          fontColor: "#444363",
+                          fontSize: 12
+                      }
+                  }
+              ],
+              xAxes: [
+                  {
+                      ticks: {
+                          fontColor: "#444363",
+                          fontSize: 12
+                      }
+                  }
+              ]
+          }
+      }
+  });
 }
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    drawChart(document.getElementById("stats-0"), 500);
+    drawChart(document.getElementById("stats-1"), 1500);
+    drawChart(document.getElementById("stats-2"), 200);
+});
